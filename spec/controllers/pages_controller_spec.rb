b393551d345cec4def78f052c5172cb5116464bd
svetla9101 <svetla.yukhnovich@gmail.com>
2018-08-08 22:18:47 +0300

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,18 +2,33 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
 
+  
   describe "GET #home" do
+    render_views
     it "returns http success" do
       get :home
-      expect(response).to have_http_status(:success)
+      #expect(response).to have_http_status(:success)
+      response.should be_success
     end
+
   end
 
   describe "GET #contact" do
+
     it "returns http success" do
       get :contact
       expect(response).to have_http_status(:success)
     end
+
+  end
+
+  describe "GET #about" do
+
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+
   end
 
 end
